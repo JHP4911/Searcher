08be10ba867ba38f1f8ac46784c8fbcdfb9d6782
Ankit Dobhal <dobhal.ankit@protonmail.com>
2019-08-06 09:41:03 +0530

    updated install.sh

--- ascii_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-apt-get install figlet
-figlet -c Google-Searcher With Python
-#Use this tool to create ascii arts
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+echo "Installtion procees of libraries"
+pip3 install requests
+pip3 install webbrowser
+pip3 install bs4
+echo "libraries installed"
